94ee481edfd9523ed39a43b7b2feb9f30c9c7db7
Matthew Ellis <42591412+ellis-matthew4@users.noreply.github.com>
2022-08-29 11:02:04 -0500

    Add project files.

---
+++ Controllers/LoginController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Http;
+
+namespace Anchor_Project.Controllers
+{
+    public class LoginController : ApiController
+    {
+        [HttpGet]
+        // GET: Login
+        public async Task<string> Login(string username, string password)
+        {
+            return "";
+        }
+    }
+}
---
+++ Controllers/ValuesController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Anchor_Project.Controllers
+{
+    public class ValuesController : ApiController
+    {
+        [HttpPost]
+        // POST a record
+        public void Post([FromBody] string value)
+        {
+        }
+
+        [HttpDelete]
+        // DELETE a record
+        public void Delete(int id)
+        {
+        }
+
+        [HttpGet]
+        // GET SS for quote_currency_amount over the entire dataset
+        public void ssAmountOverEntireSet()
+        {
+        }
+
+        [HttpGet]
+        // GET SS for type stock_buy
+        public void ssForTypeStockBuy()
+        {
+        }
+
+        [HttpGet]
+        // GET SS for each type
+        public void ssByType()
+        {
+        }
+
+        [HttpGet]
+        // GET SS for each type and quote combination
+        public void ssByTypeAndQuoteCombination()
+        {
+        }
+    }
+}
